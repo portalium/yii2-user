ffc72ed522624f5c1b8a3b2cd6502aefd9d3a994
demirmehmet0 <demirmehmet0@yahoo.com>
2022-04-09 11:54:30 +0300

    change module

--- src/Module.php
+++ src/Module.php
@@ -2,6 +2,8 @@
 
 namespace portalium\user;
 
+use portalium\site\widgets\LoginButton;
+
 class Module extends \portalium\base\Module
 {
     const EVENT_USER_CREATE = 'user-create';
@@ -11,17 +13,35 @@ class Module extends \portalium\base\Module
     const EVENT_USER_DELETE = 'user-delete';
     const EVENT_USER_DELETE_BEFORE = 'user-delete-before';
 
+    public static $description = 'User Management Module';
     public $apiRules = [
         [
             'class' => 'yii\rest\UrlRule',
             'controller' => [
-                'user/users',
+                'user/users'
             ]
         ],
     ];
 
     public static $tablePrefix = 'user_';
-    
+    public function getMenuItems(){
+        $menuItems = [
+            [
+                [
+                    'type' => 'model',
+                    'class' => 'portalium\user\models\User',
+                    'route' => '/user/default/view',
+                    'field' => [ 'id' => 'id', 'name' => 'username' ],
+                ],
+                [
+                    'type' => 'action',
+                    'route' => '/user/default/index',
+                ],
+            ],
+        ];
+        return $menuItems;
+    }
+
     public static function moduleInit()
     {
         self::registerTranslation('user','@portalium/user/messages',[
