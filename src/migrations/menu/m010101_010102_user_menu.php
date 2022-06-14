d352738b055b16e27cc64e1d44930dc474614e1f
demirmehmet0 <demirmehmet0@yahoo.com>
2022-06-14 17:42:18 +0300

    bug fixed

--- src/migrations/menu/m010101_010102_user_menu.php
+++ src/migrations/menu/m010101_010102_user_menu.php
@@ -14,11 +14,11 @@ class m010101_010102_user_menu extends Migration
         $id_menu = Menu::find()->where(['slug' => 'web-menu'])->one()->id_menu;
 
         $this->batchInsert('menu_item', ['id_item', 'label', 'slug', 'type', 'style', 'data', 'sort', 'id_parent', 'id_menu', 'name_auth', 'date_create', 'date_update'], [
-            [NULL, 'Users', 'users', '1', '{\"icon\":\"\",\"color\":\"\",\"iconSize\":\"\"}', '{\"type\":\"1\",\"data\":{\"route\":\"#\"}}', '2', '0', $id_menu, '', '2022-06-13 15:30:28', '2022-06-13 15:30:28'],
-            [NULL, 'Permissions', 'users-permissions', '2', '{\"icon\":\"\",\"color\":\"\",\"iconSize\":\"\"}', '{\"type\":\"2\",\"data\":{\"module\":\"user\",\"routeType\":\"action\",\"route\":\"\\/user\\/auth\\/permission\",\"model\":null,\"menuRoute\":null,\"menuType\":\"web\"}}', '3', '6', $id_menu, '', '2022-06-13 15:32:26', '2022-06-13 15:32:26'],
-            [NULL, 'Roles', 'users-roles', '2', '{\"icon\":\"\",\"color\":\"\",\"iconSize\":\"\"}', '{\"type\":\"2\",\"data\":{\"module\":\"user\",\"routeType\":\"action\",\"route\":\"\\/user\\/auth\\/role\",\"model\":null,\"menuRoute\":null,\"menuType\":\"web\"}}', '4', '6', $id_menu, '', '2022-06-13 15:32:26', '2022-06-13 15:32:26'],
-            [NULL, 'Groups', 'users-groups', '2', '{\"icon\":\"\",\"color\":\"\",\"iconSize\":\"\"}', '{\"type\":\"2\",\"data\":{\"module\":\"user\",\"routeType\":\"action\",\"route\":\"\\/user\\/auth\\/group\",\"model\":null,\"menuRoute\":null,\"menuType\":\"web\"}}', '5', '6', $id_menu, '', '2022-06-13 15:32:26', '2022-06-13 15:32:26'],
-            [NULL, 'Users', 'users-users', '2', '{\"icon\":\"\",\"color\":\"\",\"iconSize\":\"\"}', '{\"type\":\"2\",\"data\":{\"module\":\"user\",\"routeType\":\"action\",\"route\":\"\\/user\\/auth\\/user\",\"model\":null,\"menuRoute\":null,\"menuType\":\"web\"}}', '6', '6', $id_menu, '', '2022-06-13 15:32:26', '2022-06-13 15:32:26'],
+            [NULL, 'Users', 'users', '1', '{"icon":"","color":"","iconSize":""}', '{"type":"1","data":{"route":"#"}}', '2', '0', $id_menu, '', '2022-06-13 15:30:28', '2022-06-13 15:30:28'],
+            [NULL, 'Permissions', 'users-permissions', '2', '{"icon":"","color":"","iconSize":""}', '{"type":"2","data":{"module":"user","routeType":"action","route":"\\/user\\/auth\\/permission","model":null,"menuRoute":null,"menuType":"web"}}', '3', '2', $id_menu, '', '2022-06-13 15:32:26', '2022-06-13 15:32:26'],
+            [NULL, 'Roles', 'users-roles', '2', '{"icon":"","color":"","iconSize":""}', '{"type":"2","data":{"module":"user","routeType":"action","route":"\\/user\\/auth\\/role","model":null,"menuRoute":null,"menuType":"web"}}', '4', '2', $id_menu, '', '2022-06-13 15:32:26', '2022-06-13 15:32:26'],
+            [NULL, 'Groups', 'users-groups', '2', '{"icon":"","color":"","iconSize":""}', '{"type":"2","data":{"module":"user","routeType":"action","route":"\\/user\\/group","model":null,"menuRoute":null,"menuType":"web"}}', '5', '2', $id_menu, '', '2022-06-13 15:32:26', '2022-06-13 15:32:26'],
+            [NULL, 'Users', 'users-users', '2', '{"icon":"","color":"","iconSize":""}', '{"type":"2","data":{"module":"user","routeType":"action","route":"\\/user\\/default\\/index","model":null,"menuRoute":null,"menuType":"web"}}', '6', '2', $id_menu, '', '2022-06-13 15:32:26', '2022-06-13 15:32:26'],
         ]);
     }
 
