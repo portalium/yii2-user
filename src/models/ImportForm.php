6443a56259ecefadddd09d3f72eb61a86db812e7
irmakyuceak <irmakyuceak0803@gmail.com>
2024-10-24 08:52:43 +0000

    feat: A function was written that automatically retrieves the column names on the import page.

--- src/models/ImportForm.php
+++ src/models/ImportForm.php
@@ -9,11 +9,6 @@ class ImportForm extends Model
 {
 
     public $file;
-    public $first_name;
-    public $last_name;
-    public $username;
-    public $email;
-    public $password;
     public $group;
     public $role;
 
@@ -24,13 +19,7 @@ class ImportForm extends Model
     {
         return [
             [['file'], 'file', 'skipOnEmpty' => false, 'extensions' => 'csv'],
-            [['username', 'email'], 'required'],
-            ['email', 'required'],
-            [['username', 'group', 'role'], 'string', 'max' => 40],
-            ['password', 'string', 'max' => 40],
-            ['first_name', 'string', 'max' => 40],
-            ['last_name', 'string', 'max' => 40],
-
+            [['group', 'role'], 'string', 'max' => 40],
         ];
     }
 
@@ -41,11 +30,6 @@ class ImportForm extends Model
     {
         return [
             'file' => Module::t('File'),
-            'first_name' => Module::t('First Name Column Name'),
-            'last_name' => Module::t('Last Name Column Name'),
-            'username' => Module::t('Username Column Name'),
-            'email' => Module::t('Email Column Name'),
-            'password' => Module::t('Password Column Name')
         ];
     }
 
